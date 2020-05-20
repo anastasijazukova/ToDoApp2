270e755cc2f3d3af86826fc03610b97cd3271771
Anastasija Zukova <>
2020-05-20 22:49:27 +0300

    no message

---
+++ ToDoApp/VC/SecondViewController.swift
@@ -0,0 +1,74 @@
+//
+//  AddToDoViewController.swift
+//  ToDoApp
+//
+//  Created by anastasija.zukova on 20/05/2020.
+//  Copyright © 2020 Accenture. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    @IBOutlet weak var dateInput: UIDatePicker!
+    @IBOutlet weak var textInput: UITextView!
+    @IBOutlet weak var doneButton: UIButton!
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+// MARK: Add keyboard by using appropriate method instead of this:
+        /*NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(with:)), name: .UIKeyboardWillShow, object: nil)
+         and this:
+         [[NotificationCenter.default] addObserver:self
+         selector:@selector(keyboardWillBeHidden:)
+         name:UIKeyboardWillHideNotification object:nil];
+         */
+        
+        textInput.becomeFirstResponder()
+    }
+    @objc func keyboardWillShow(with notification:Notification) {
+        guard let keyboardFrame = notification.userInfo?[""] as? NSValue else { return }
+        let keyboardHeight = keyboardFrame.cgRectValue.height
+        
+        bottomConstraint.constant = keyboardHeight
+    }
+    
+    @IBAction func cancel(_ sender: UIButton) {
+        dismiss(animated: true)
+        textInput.resignFirstResponder()
+    }
+    @IBAction func done(_ sender: UIButton) {
+    }
+    
+    //
+    
+    
+    // Do any additional setup after loading the view.
+}
+
+// MARK: Done button extension (for use with initially hidden "done" button)
+/* extension SecondViewController: UITextViewDelegate {
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if doneButton.isHidden {
+            doneButton.isHidden = false
+        }
+    }
+}
+*/
+/*
+ // MARK: - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+ // Get the new view controller using segue.destination.
+ // Pass the selected object to the new view controller.
+ }
+ */
+
--- ToDoApp/VC/ToDoTableView.swift
+++ ToDoApp/VC/ToDoTableView.swift
@@ -10,7 +10,22 @@ import UIKit
 
 class ToDoTableView: UITableView {
 
+    
+    override func numberOfRows(inSection section: Int) -> Int {
+        return 10
+    }
+    
+    //override func dequeueReusableCell(withIdentifier identifier: String, for indexPath: IndexPath) -> UITableViewCell {
+    
     /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    
+     
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
