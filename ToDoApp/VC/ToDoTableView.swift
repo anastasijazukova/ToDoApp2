b5b60bce1bef9b408ea17e1f61a30deb93f83b71
Anastasija Zukova <>
2020-05-20 15:57:56 +0300

    latest

---
+++ ToDoApp/VC/ToDoTableView.swift
@@ -0,0 +1,21 @@
+//
+//  ToDoTableView.swift
+//  ToDoApp
+//
+//  Created by anastasija.zukova on 20/05/2020.
+//  Copyright © 2020 Accenture. All rights reserved.
+//
+
+import UIKit
+
+class ToDoTableView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
