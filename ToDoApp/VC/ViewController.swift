e4669f0732fec5569562c94388f08a534062e98c
Anastasija Zukova <>
2020-05-21 12:04:34 +0300

    no message

--- ToDoApp/VC/ViewController.swift
+++ ToDoApp/VC/ViewController.swift
@@ -5,20 +5,48 @@
 //  Created by anastasija.zukova on 20/05/2020.
 //  Copyright © 2020 Accenture. All rights reserved.
 //
-
+import Foundation
 import UIKit
+
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        tableView.delegate = self
+        tableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
+}
+
+extension ViewController: UITableViewDelegate {
+    func dequeueReusableCell(withIdentifier identifier: String, for indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoCell", for: indexPath)
+    return cell
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
     
 
     /*
-    // MARK: - Navigation
+     @IBOutlet weak var tableView: ToDoTableView!
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -27,4 +55,4 @@ class ViewController: UIViewController {
     }
     */
 
-}
+
